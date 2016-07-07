e5f435b7a86e4bce4b39d3e861afdd22a0d9d7b4
Jack Machin <jack.machin125@gmail.com>
2016-07-07 14:52:15 +0100

    PRoblems

--- page-group.php
+++ page-group.php
@@ -30,8 +30,11 @@
                                     </div>
 
 				        <div class="ifa-map">
-			                 <a id="rutherford-wilkinson" onclick="selectThis('rutherford-wilkinson')" href="javascript:void(0);" class="bullet preview">Rutherford Wilkinson></a>
-                            <img src="http://www.pfgl.co.uk/corporate-solutions/files/2016/02/RWltd_chartered-300x148.jpg" width="100" height="52" class="map-icon" id="map-logo-rutherford">
+                            <div id="rutherford-wilkinson">
+                            			                 <a onclick="selectThis('rutherford-wilkinson')" href="javascript:void(0);" class="bullet preview">Rutherford Wilkinson></a>
+                            <img src="http://www.pfgl.co.uk/corporate-solutions/files/2016/02/RWltd_chartered-300x148.jpg" class="map-icon" id="map-logo-rutherford">
+
+                            </div>
 
 			<a id="harrogate-independent"  onclick="selectThis('harrogate-independent')" href="javascript:void(0);" class="bullet preview">Harrogate Independent Financial Services</a>
 			<a id="multiplex"  onclick="selectThis('multiplex')" href="javascript:void(0);" class="bullet preview">Multiplex</a>
