ff8011852e9abe8bddf4c0faadf3bf1b164972c6
Jack Machin <jack.machin125@gmail.com>
2016-08-08 13:38:01 +0100

    Work man

--- page-group.php
+++ page-group.php
@@ -31,29 +31,29 @@
 
 				        <div class="ifa-map">
                              <a id="rutherford-wilkinson" onclick="selectThis('rutherford-wilkinson')" href="javascript:void(0);" class="bullet preview">Rutherford Wilkinson>
-                                <img src="http://www.pfgl.co.uk/corporate-solutions/files/2016/02/RWltd_chartered-300x148.jpg" class="map-logo" id="map-logo-rutherford">
+                                <img src="http://www.perspectivecorporate.co.uk/files/2016/02/RWltd_chartered-300x148.jpg" class="map-logo" id="map-logo-rutherford">
                             </a>
 
                             <a id="multiplex"  onclick="selectThis('multiplex')" href="javascript:void(0);" class="bullet preview">Multiplex
-                                <img class="map-logo" id="map-logo-multiplex" src="http://www.pfgl.co.uk/corporate-solutions/files/2016/02/multiplex-logo-300x86.jpg" alt="multiplex logo">
+                                <img class="map-logo" id="map-logo-multiplex" src="http://www.perspectivecorporate.co.uk/files/2016/02/multiplex-logo-300x86.jpg" alt="multiplex logo">
                             </a>
 
                             <a id="wingham-wyatt" onclick="selectThis('wingham-wyatt')" href="javascript:void(0);" class="bullet preview">Wingham Wyatt Financial Services Ltd
 
-                                <img class="map-logo" id="map-logo-wingham" src="http://www.pfgl.co.uk/corporate-solutions/files/2016/02/WW-300x163.jpg" alt="Microsoft Word - WWDoc Logo _2_">
+                                <img class="map-logo" id="map-logo-wingham" src="http://www.perspectivecorporate.co.uk/files/2016/02/WW-300x163.jpg" alt="Microsoft Word - WWDoc Logo _2_">
                             </a>
 
                             <a id="perspective-financial" onclick="selectThis('perspective-financial')"  href="javascript:void(0);" class="bullet preview">Perspective Financial Management Ltd
-                                <img class="map-logo" id="map-logo-pfm" src="http://www.pfgl.co.uk/corporate-solutions/files/2016/02/PWM-logo-white-300x61.jpg" alt="Print" >
+                                <img class="map-logo" id="map-logo-pfm" src="http://www.perspectivecorporate.co.uk/files/2016/02/PWM-logo-white-300x61.jpg" alt="Print" >
                             </a>
 
 
                             <a id="equilibrium" onclick="selectThis('equilibrium')" href="javascript:void(0);" class="bullet preview">Equilibrium Wealth Management Limited
-                                <img class="map-logo" id="map-logo-equi" src="http://www.pfgl.co.uk/corporate-solutions/files/2016/02/EWM_logosmall1-300x170.jpg" alt="EWM_logo(small)1">
+                                <img class="map-logo" id="map-logo-equi" src="http://www.perspectivecorporate.co.uk/files/2016/02/EWM_logosmall1-300x170.jpg" alt="EWM_logo(small)1">
                             </a>
 
                             <a id="shirebrook" onclick="selectThis('shirebrook')" href="javascript:void(0);" class="bullet preview">Shirebrook Wealth Management Limited
-                                <img class="map-logo" id="map-logo-shirebrook" src="http://www.pfgl.co.uk/corporate-solutions/files/2016/02/ShirebrookLogo-300x102.gif" alt="ShirebrookLogo">
+                                <img class="map-logo" id="map-logo-shirebrook" src="http://www.perspectivecorporate.co.uk/files/2016/02/ShirebrookLogo-300x102.gif" alt="ShirebrookLogo">
                             </a>
 
 		                  </div><!--ifa-map-->
